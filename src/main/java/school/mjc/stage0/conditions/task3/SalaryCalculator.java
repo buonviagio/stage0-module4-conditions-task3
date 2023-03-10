a78da2e4ff51f1ada596bc08d1c40be0ecbb4474
Shapovalov Dmytro <buonviagio@gmail.com>
2023-03-10 16:50:20 +0200

    My first commit Module4.1 Task3

--- src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
+++ src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
@@ -2,5 +2,15 @@ package school.mjc.stage0.conditions.task3;
 
 public class SalaryCalculator {
     public void calculateSalary(int salary) {
+        double tmp = 100;
+        if(salary <= 10_000){
+            System.out.println(salary / tmp * 85);
+        } else if(salary > 10_000 && salary <= 20_000 ){
+            System.out.println(salary / tmp * 82);
+        } else if(salary > 20_000){
+            System.out.println(salary / tmp * 80);
+        } else if (salary < 0){
+            System.out.println("wrong input!");
+        }
     }
 }
